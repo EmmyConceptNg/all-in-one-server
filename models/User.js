c61b3d9b2c49c8f33e8814a547b8d406c14fa888
Lumidee89 <97644187+Lumidee89@users.noreply.github.com>
2024-10-02 17:52:06 +0100

    first commit

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isVerified: { type: Boolean, default: false },
+    otp: { type: String },  
+    otpCreatedAt: { type: Date },
+    passwordResetToken: { type: String }, 
+    passwordResetExpires: { type: Date },
+});
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+UserSchema.methods.comparePassword = async function (inputPassword) {
+    return await bcrypt.compare(inputPassword, this.password);
+};
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
