89c07d5c039c125e59fdeb54bb27316dbf046bf1
Lumidee89 <97644187+Lumidee89@users.noreply.github.com>
2024-10-02 20:19:47 +0100

    ok

--- config/smtp.js
+++ config/smtp.js
@@ -1,12 +1,12 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST, // e.g., smtp.yourdomain.com
-  port: process.env.SMTP_PORT, // e.g., 587 or 465
-  secure: process.env.SMTP_SECURE === 'true', // true for port 465, false for port 587
+  host: process.env.SMTP_HOST, 
+  port: process.env.SMTP_PORT, 
+  secure: process.env.SMTP_SECURE === 'true', 
   auth: {
-    user: process.env.SMTP_USER, // Your SMTP username
-    pass: process.env.SMTP_PASS, // Your SMTP password
+    user: process.env.SMTP_USER, 
+    pass: process.env.SMTP_PASS, 
   },
 });
 
