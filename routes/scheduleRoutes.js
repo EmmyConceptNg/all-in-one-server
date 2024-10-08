a03b5fe6416f88bb4e5621734cb823d78cbf07cc
Lumidee89 <97644187+Lumidee89@users.noreply.github.com>
2024-10-08 10:40:40 +0100

    Update scheduleRoutes.js

--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const scheduleController = require('../controllers/scheduleController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
-router.post('/add', authMiddleware(['super_admin', 'manager']), scheduleController.addSchedule);
+router.post('/add', authMiddleware(['staff', 'super_admin', 'manager']), scheduleController.addSchedule);
 router.get('/user', authMiddleware(['super_admin', 'manager', 'staff']), scheduleController.getUserSchedules);
 router.delete('/:id', authMiddleware(['super_admin', 'manager']), scheduleController.deleteSchedule);
 
