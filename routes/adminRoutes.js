8672125cde2d9a5983fc84eea095bb3f34ed540a
Emmy <emmyconceptng@gmail.com>
2025-02-04 16:15:41 +0100

    admin routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,8 +1,9 @@
 const authController = require("../controllers/admin/authController");
 const { getAllSuperAdmin, enableSuperAdmin, disableSuperAdmin, getSingleSuperAdmin } = require("../controllers/admin/superAdminController");
 const authAdminMiddleware = require("../middlewares/authAdminMiddleware");
-router.post("/login", authController);
-router.post("/register", authController);
+const router = require("./auth");
+router.post("/login", authController.login);
+router.post("/register", authController.register);
 router.post("/clients",authAdminMiddleware(['admin']), getAllSuperAdmin);
 router.post("/clients/disable",authAdminMiddleware(['admin']), disableSuperAdmin);
 router.post("/clients/enable",authAdminMiddleware(['admin']), enableSuperAdmin);
