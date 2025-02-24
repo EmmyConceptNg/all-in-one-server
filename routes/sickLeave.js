0319355e87b8f4332c458a5ce26c4072c2f9438a
Emmy <emmyconceptng@gmail.com>
2025-02-24 08:25:54 +0100

    fix missing sickleave route on server.js

--- routes/sickLeave.js
+++ routes/sickLeave.js
@@ -5,23 +5,23 @@ const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/create', 
   authMiddleware(['staff', 'manager', 'super_admin']), 
-  SickLeaveController.requestVacation
+  SickLeaveController.requestSickLeave
 );
 
 router.put('/:id', 
   authMiddleware(['super_admin', 'manager', 'staff']), 
-  SickLeaveController.updateVacation
+  SickLeaveController.updateSickLeave
 );
 
 // Simplified route that uses query parameters instead
 router.get('/', 
   authMiddleware(['super_admin', 'manager', 'staff']), 
-  SickLeaveController.getVacations
+  SickLeaveController.getSickLeaves
 );
 
 router.delete('/:id', 
   authMiddleware(['super_admin', 'manager', 'staff']), 
-  SickLeaveController.deleteVacation
+  SickLeaveController.deleteSickLeave
 );
 
 module.exports = router;
